1606d95b62bae75b7108a536376317d5e6cb9c60
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-08 15:48:47 +0100

    update

--- app/views/layout.php
+++ app/views/layout.php
@@ -4,13 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo ASSETS; ?>css/style.css">
+    <link rel="icon" type="image/x-icon" href="<?php echo ASSETS; ?>picture/cinema.ico">
     <title><?php echo $title ?? "Cinetech" ?></title>
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <a href="<?php echo HOST; ?>">
+                <img src="<?php echo ASSETS; ?>picture/cinema.png" alt="logo">
+            </a>i
+        </div>
+        <nav>
+
+            <ul class="navLink">
+                <li><a class="navItems" href="<?php echo HOST; ?>">Accueil</a></li>
+                <li><a class="navItems" href="<?php echo HOST; ?>film">Films</a></li>
+                <li><a class="navItems" href="<?php echo HOST; ?>serie">Série</a></li>
+            </ul>
+            <ul class="navLink">
+                <li><a class="navItems" href="<?php echo HOST; ?>login">Connexion</a></li>
+                <li><a class="navItems" href="<?php echo HOST; ?>register">Inscription</a></li>
+            </ul>
+        </nav>
+    </header>
 
     <main>
-        <?php echo $contentPage; ?>
+        <?php echo $contentPage; ?>  
     </main>
 
+    <script>
+        const filmUrl = <?php echo json_encode($url); ?>;
+        const serieUrl = <?php echo json_encode($url); ?>;
+    </script>
+    <script src="<?php echo ASSETS; ?>js/film.js"></script>
+    <script src="<?php echo ASSETS; ?>js/serie.js"></script>
 </body>
 </html>
