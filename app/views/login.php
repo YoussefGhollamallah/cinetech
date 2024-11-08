5b70a00797baeb5bedad6e96823656cca2177964
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-08 09:45:00 +0100

    création de la vue de connexion

---
+++ app/views/login.php
@@ -0,0 +1,10 @@
+<section>
+    <h1>Login</h1>
+    <form action="login" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+    </form>
+
+    <p>Don't have an account? <a href="register">Register</a></p>
+</section>
