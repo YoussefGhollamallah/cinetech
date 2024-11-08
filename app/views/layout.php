106b023df9b6cdcd4939319f87af9951236f68e7
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-08 09:44:34 +0100

    création de la vue layout qui va récupérer toutes les vues

---
+++ app/views/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo ASSETS; ?>css/style.css">
+    <title><?php echo $title ?? "Cinetech" ?></title>
+</head>
+<body>
+
+    <main>
+        <?php echo $contentPage; ?>
+    </main>
+
+</body>
+</html>
