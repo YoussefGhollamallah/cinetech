e7ad49576861f3fc777cea3bfb52c9d4873c1be7
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-08 15:48:31 +0100

    mise en place des vues

---
+++ app/views/film.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once config . 'api.php';
+$url = API_FILM_TENDANCE_URL;
+
+?>
+<section>
+    <h1>Films</h1>
+
+    <div id="film"></div>
+</section>
---
+++ app/views/serie.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once config . 'api.php';
+$url = API_SERIE_TENDANCE_URL;
+
+?>
+
+<section>
+    <h1>Séries</h1>
+
+    <div id="serie"></div>
+</section>
