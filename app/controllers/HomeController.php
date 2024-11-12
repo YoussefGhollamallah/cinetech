ed02591823bc8eeaf5b5a206c7dbc6ecfdcedbe1
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-12 16:19:18 +0100

    update du Homecontroller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,56 +1,54 @@
 <?php
-
-// Path: app/controllers/HomeController.php
-
 class HomeController
 {
     public function home()
     {
         $title = "Cinetech";
-        $view = new View("index");
-        $view->setVars(["title" => $title]);
-        $view->render();
+        $this->renderView("index", ["title" => $title]);
     }
 
     public function erreur404()
     {
         $title = "Erreur 404";
-        $view = new View("404Erreur");
-        $view->setVars(["title" => $title]);
-        $view->render();
+        $this->renderView("404Erreur", ["title" => $title]);
     }
 
     public function register()
     {
         $title = "Inscription";
-        $view = new View("register");
-        $view->setVars(["title" => $title]);
-        $view->render();
+        $this->renderView("register", ["title" => $title]);
     }
 
     public function login()
     {
         $title = "Connexion";
-        $view = new View("login");
-        $view->setVars(["title" => $title]);
-        $view->render();
+        $this->renderView("login", ["title" => $title]);
     }
 
     public function film()
     {
         $title = "Films";
-        $view = new View("film");
-        $view->setVars(["title" => $title]);
-        $view->render();
+        $this->renderView("film", ["title" => $title]);
     }
 
     public function serie()
     {
         $title = "Série";
-        $view = new View("serie");
-        $view->setVars(["title" => $title]);
-        $view->render();
+        $this->renderView("serie", ["title" => $title]);
+    }
+
+    public function profile()
+    {
+        $title = "Profil";
+        $this->renderView("profile", ["title" => $title]);
     }
 
 
+
+    public function renderView($viewName, $vars = [])
+    {
+        $view = new View($viewName);
+        $view->setVars($vars);
+        $view->render();
+    }
 }
