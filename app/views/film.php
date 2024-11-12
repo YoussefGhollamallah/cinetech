483a52626970b037cdee58811cedc52f306e8f5a
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-12 16:15:48 +0100

    affichage des films et série

--- app/views/film.php
+++ app/views/film.php
@@ -1,11 +1,19 @@
 <?php
 
-require_once config . 'api.php';
+require_once config . "api.php";
+
 $url = API_FILM_TENDANCE_URL;
 
 ?>
+
 <section>
     <h1>Films</h1>
 
     <div id="film"></div>
 </section>
+
+<script>
+    var $filmUrl = "<?php echo $url; ?>";
+</script>
+
+<script src="<?php echo ASSETS; ?>js/film.js"></script>
--- app/views/serie.php
+++ app/views/serie.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once config . 'api.php';
-$url = API_SERIE_TENDANCE_URL;
+require_once config . "api.php";
+
+$urlSerie = API_SERIE_TENDANCE_URL;
 
 ?>
 
 <section>
     <h1>Séries</h1>
-
     <div id="serie"></div>
 </section>
+
+<script src="<?php echo ASSETS;?>js/serie.js"></script>
