9a23f1bb512dce70344c7e94a0f1c62bef4099f7
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-12 16:17:29 +0100

    ajout d'une petite div pour afficher bienvenue ou bonjour user si connecté

--- app/views/layout.php
+++ app/views/layout.php
@@ -1,3 +1,27 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+function deconnexion()
+{
+    session_unset();
+    session_destroy();
+    header('Location:'. BASE_URL . 'index');
+    exit();
+}
+
+if (isset($_GET['action']) && $_GET['action'] === 'deconnexion') {
+    deconnexion();
+}
+
+require_once config . "api.php";
+
+$url = API_FILM_TENDANCE_URL;
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -12,7 +36,7 @@
         <div class="logo">
             <a href="<?php echo HOST; ?>">
                 <img src="<?php echo ASSETS; ?>picture/cinema.png" alt="logo">
-            </a>i
+            </a>
         </div>
         <nav>
 
@@ -21,22 +45,36 @@
                 <li><a class="navItems" href="<?php echo HOST; ?>film">Films</a></li>
                 <li><a class="navItems" href="<?php echo HOST; ?>serie">Série</a></li>
             </ul>
-            <ul class="navLink">
-                <li><a class="navItems" href="<?php echo HOST; ?>login">Connexion</a></li>
-                <li><a class="navItems" href="<?php echo HOST; ?>register">Inscription</a></li>
-            </ul>
+
+            <?php if (isset($_SESSION['user'])): ?>
+                <ul class="navLink">
+                    <li><a class="navItems" href="<?php echo HOST; ?>profile">Profil</a></li>
+                    <li><a class="navItems" href="<?php echo HOST; ?>index?action=deconnexion">Déconnexion</a></li>
+                </ul>
+            <?php else: ?>
+                <ul class="navLink">
+                    <li><a class="navItems" href="<?php echo HOST; ?>login">Connexion</a></li>
+                    <li><a class="navItems" href="<?php echo HOST; ?>register">Inscription</a></li>
+                </ul>
+            <?php endif; ?>
+
         </nav>
     </header>
 
+    <div class="banner">
+
+        <?php if(isset($_SESSION["user"])) :?>
+            <p>Bonjour <?php echo $_SESSION["user"]["firstname"] . " " . $_SESSION["user"]["lastname"]; ?></p>
+        <?php else: ?>
+            <p>Bienvenue sur Cinetech</p>
+        <?php endif; ?>
+
+    </div>
+    
     <main>
         <?php echo $contentPage; ?>  
     </main>
 
-    <script>
-        const filmUrl = <?php echo json_encode($url); ?>;
-        const serieUrl = <?php echo json_encode($url); ?>;
-    </script>
-    <script src="<?php echo ASSETS; ?>js/film.js"></script>
-    <script src="<?php echo ASSETS; ?>js/serie.js"></script>
+
 </body>
 </html>
