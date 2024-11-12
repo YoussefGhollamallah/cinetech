45a2e6027a3d509139b836f06dc0011544b14bdd
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-12 16:16:23 +0100

    mise a jour de la vue de connexion

--- app/views/login.php
+++ app/views/login.php
@@ -1,3 +1,32 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+$user = new UserController();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($_POST['username']) && !empty($_POST['password'])) {
+        $userdata = $user->login($_POST['username'], $_POST['password']);
+        if ($userdata){
+            $_SESSION['user'] = $userdata;
+            header('Location: profile');
+            exit();
+        } else {
+            echo 'Email ou mot de passe incorrect';
+        }
+    }
+}
+
+if(isset($_SESSION['user'])) {
+    header('Location: profile');
+    exit();
+}
+
+?>
+
 <section>
     <h1>Login</h1>
     <form action="" class="register" method="post">
