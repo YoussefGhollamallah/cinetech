7b6b5e25e5e6b72c85c505344be88467203a28f9
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-14 16:55:36 +0100

    Ajout : mise en place du routeur pour récurérer les éléments de l'url pour pouvoir l'afficher

--- classes/Routeur.php
+++ classes/Routeur.php
@@ -1,4 +1,5 @@
 <?php
+
 class Routeur
 {
     private $request;
@@ -10,6 +11,7 @@ class Routeur
         "login" => ["controller" => "HomeController", "method" => "login"],
         "film" => ["controller" => "HomeController", "method" => "film"],
         "serie" => ["controller" => "HomeController", "method" => "serie"],
+        "detail" => ["controller" => "HomeController", "method" => "detail"],
         "profile" => ["controller" => "HomeController", "method" => "profile"],
     ];
 
@@ -21,53 +23,44 @@ class Routeur
     public function renderController()
     {
         $request = $this->request;
-    
+        
         if (array_key_exists($request, $this->routes)) {
-    
-            
             $controller = $this->routes[$request]["controller"];
             $method = $this->routes[$request]["method"];
-    
+        
             if (class_exists($controller)) {
                 $controllerInstance = new $controller();
-    
-                // Récupérer les paramètres depuis l'URL pour 'detail'
+        
+                // Gestion de la route 'detail' avec paramètres extraits de l'URL
                 if ($request === 'detail') {
-                    // Exemple d'URL : /detail/94605/serie
-                    $urlParts = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-                    if (count($urlParts) === 3) {
-                        $id = $urlParts[1];  // Premier paramètre (id)
-                        $type = $urlParts[2]; // Deuxième paramètre (type)
-                        
-                        // Valider l'ID et le type
-                        if (is_numeric($id) && ($type === 'film' || $type === 'serie')) {
-                            if (method_exists($controllerInstance, $method)) {
-                                $controllerInstance->$method($id, $type); // Appel de la méthode avec l'ID et le type
-                            } else {
-                                $this->redirect404();
-                            }
-                        } else {
-                            $this->redirect404();  // ID ou type invalide
+                    // Extraire les paramètres de l'URL
+                    $urlParts = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
+                 
+                    // Vérifiez qu'il y a bien suffisamment de parties dans l'URL et que l'ID est valide
+                    if (count($urlParts) >= 4 && is_numeric($urlParts[2]) && in_array($urlParts[3], ['film', 'serie'])) {
+                        $id = $urlParts[2];   // L'ID se trouve à l'index 2
+                        $type = $urlParts[3]; // Le type (film ou série) se trouve à l'index 3
+                
+                        // Appel de la méthode du contrôleur
+                        if (method_exists($controllerInstance, $method)) {
+                            $controllerInstance->$method($id, $type);
+                            return;
                         }
-                    } else {
-                        $this->redirect404();  // Structure d'URL incorrecte
                     }
+            
+                
                 } else {
-                    // Récupérer sans paramètres supplémentaires
                     if (method_exists($controllerInstance, $method)) {
                         $controllerInstance->$method();
-                    } else {
-                        $this->redirect404();
+                        return;
                     }
                 }
-            } else {
-                $this->redirect404();
             }
-        } else {
-            $this->redirect404();
         }
+        $this->redirect404();
     }
-
+    
+    // Méthode pour rediriger vers la page 404
     public function redirect404()
     {
         $controller = $this->routes["404Erreur"]["controller"];
