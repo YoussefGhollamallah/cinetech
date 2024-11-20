f002b4ba00f69232c794b235f768a7a7ad183a87
YoussefGhollamallah <youssef.gh@live.fr>
2024-11-20 16:32:49 +0100

    mise en place du style et du js pour le menu burger

---
+++ src/js/burger.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("burger");
+    const nav = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+});
